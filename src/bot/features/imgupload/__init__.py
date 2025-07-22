86a8e5b0f78295219b6b1596007fbf1b960ce868
undefined_name <70478422+undefined-name12@users.noreply.github.com>
2025-07-22 19:57:04 +0200

    Update __init__.py

--- src/bot/features/imgupload/__init__.py
+++ src/bot/features/imgupload/__init__.py
@@ -1,9 +1,5 @@
 """
 tgrambuddy/src/bot/features/imgupload/__init__.py
-
-TgramBuddy - A solution for building and managing Telegram bots.
-Copyright (c) 2025 Maks V. Zaikin
-Released by 01-May-2025 under the MIT License.
 """
 from .imgupload_router import imgupload_router
 
