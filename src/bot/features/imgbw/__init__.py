b3aa025d6541df74ed2aee719562bb8e7e0b8339
undefined_name <70478422+undefined-name12@users.noreply.github.com>
2025-07-22 19:55:52 +0200

    Update __init__.py

--- src/bot/features/imgbw/__init__.py
+++ src/bot/features/imgbw/__init__.py
@@ -1,9 +1,5 @@
 """
 tgrambuddy/src/bot/features/imgbw/__init__.py
-
-TgramBuddy - A solution for building and managing Telegram bots.
-Copyright (c) 2025 Maks V. Zaikin
-Released by 01-May-2025 under the MIT License.
 """
 from .imgbw_router import imgbw_router
 
